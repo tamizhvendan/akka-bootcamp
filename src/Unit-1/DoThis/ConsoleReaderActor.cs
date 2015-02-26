1b641863485c8b882538003272a8908f8b01089d
Tamizhvendan S <tamizh88@gmail.com>
2015-02-26 12:24:44 +0530

    Completed Lesson1

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -3,37 +3,39 @@ using Akka.Actor;
 
 namespace WinTail
 {
-    /// <summary>
-    /// Actor responsible for reading FROM the console. 
-    /// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
-    /// </summary>
-    class ConsoleReaderActor : UntypedActor
-    {
-        public const string ExitCommand = "exit";
-        private ActorRef _consoleWriterActor;
+	/// <summary>
+	/// Actor responsible for reading FROM the console. 
+	/// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
+	/// </summary>
+	class ConsoleReaderActor : UntypedActor
+	{
+		public const string ExitCommand = "exit";
+		private ActorRef _consoleWriterActor;
 
-        public ConsoleReaderActor(ActorRef consoleWriterActor)
-        {
-            _consoleWriterActor = consoleWriterActor;
-        }
+		public ConsoleReaderActor(ActorRef consoleWriterActor)
+		{
+			_consoleWriterActor = consoleWriterActor;
+		}
 
-        protected override void OnReceive(object message)
-        {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
-            {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
-                return;
-            }
+		protected override void OnReceive(object message)
+		{
+			var read = Console.ReadLine();
+			if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+			{
+				// shut down the system (acquire handle to system via
+				// this actors context)
+				Context.System.Shutdown();
+				return;
+			}
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
+			// send input to the console writer to process and print
+			// YOU NEED TO FILL IN HERE
+			_consoleWriterActor.Tell(read);
 
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
-        }
+			// continue reading messages from the console
+			// YOU NEED TO FILL IN HERE
+			Self.Tell("continue");
+		}
 
-    }
+	}
 }
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -2,46 +2,52 @@
 
 namespace WinTail
 {
-    #region Program
-    class Program
-    {
-        public static ActorSystem MyActorSystem;
+	using Akka.Actor;
 
-        static void Main(string[] args)
-        {
-            // initialize MyActorSystem
-            // YOU NEED TO FILL IN HERE
+	#region Program
+	class Program
+	{
+		public static ActorSystem MyActorSystem;
 
-            PrintInstructions();
+		static void Main(string[] args)
+		{
+			// initialize MyActorSystem
+			// YOU NEED TO FILL IN HERE
+			MyActorSystem = ActorSystem.Create("MyActorSystem");
+			PrintInstructions();
 
-            // time to make your first actors!
-            //YOU NEED TO FILL IN HERE
-            // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
-            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+			// time to make your first actors!
+			//YOU NEED TO FILL IN HERE
+			// make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
+			// make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+			var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
+			var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
 
 
-            // tell console reader to begin
-            //YOU NEED TO FILL IN HERE
 
-            // blocks the main thread from exiting until the actor system is shut down
-            MyActorSystem.AwaitTermination();
-        }
 
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
-    }
-    #endregion
+			// tell console reader to begin
+			consoleReaderActor.Tell("start");
+
+			// blocks the main thread from exiting until the actor system is shut down
+			MyActorSystem.AwaitTermination();
+		}
+
+		private static void PrintInstructions()
+		{
+			Console.WriteLine("Write whatever you want into the console!");
+			Console.Write("Some lines will appear as");
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write(" red ");
+			Console.ResetColor();
+			Console.Write(" and others will appear as");
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write(" green! ");
+			Console.ResetColor();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+		}
+	}
+	#endregion
 }
